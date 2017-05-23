089e7367ee10020a6f44e88cf6e280d124141f53
j1nz <lethethangdn@gmail.com>
2017-05-23 13:07:58 +0700

    [gactruyen][2017/05/23][add]_wfe setting read mode

---
+++ wfe/js/change_font_size.js
@@ -0,0 +1,30 @@
+//hàm thay đổi chuỗi
+//function str_replace(haystack, needle, replacement) {
+//    var temp = haystack.split(needle);
+//    return temp.join(replacement);
+//}
+
+// lấy font chữ mực định
+var default_font_size = $('#test-change-size').css('fontSize');
+
+//những phần tử sẽ thực hiện khi chức năng được gọi
+var elm = $('#test-change-size');
+
+// ấn định kích thước font chữ mặc định và xóa bỏ px từ giá trị
+//var size = str_replace(reset, 'px', '');
+
+//Change font
+function fn_change_size() {
+
+	var new_size = $('#changeSize').val();
+
+	elm.css({'fontSize' : new_size});
+	//return false;
+}
+
+// khôi phục lại kích thước mặc định
+$('#resetSize').click(function () {
+
+    // ấn định kích thước font chữ mặc định
+    elm.css({'fontSize' : default_font_size});
+});
